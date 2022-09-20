9b6566875c39e729f0c0e7c9cfe82f32352b8645
yash <yashwable12@gmail.com>
2022-09-20 23:34:42 +0530

    added usestate

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
 import Logo from "../assets/logo.svg"
+import { useState } from 'react';
 
 function Register() {
+  const [values,setValues] = useState({
+    username : "",
+    email : "",
+    password : "",
+    confirmPassword : ""
+  })
+
   const handleSubmit = (event) => {
     event.preventDefault();
     alert('form');
   };
   
   const handleChange = (event) => {
-
+    setValues({...values,[event.target.name] : event.target.value }); 
   };
 
   return (
