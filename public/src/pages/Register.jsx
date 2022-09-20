d4dfadec8dd752c89e6ab7c4fe4f3629bc85f842
yash <yashwable12@gmail.com>
2022-09-20 19:58:08 +0530

    styled register button & all

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -96,6 +96,30 @@ const FormContainer = styled.div`
       }
     }
   }
+  button {
+    background-color : #997af0;
+    color : white ;
+    padding : 1rem 2rem ;
+    border : none;
+    font-weight:bold ;
+    cursor : pointer;
+    border-radius : 0.4rem;
+    font-size : 1rem;
+    text-transformation :uppercase ;
+    transition : 0.5s ease-in-out;
+    &:hover {
+      background-color : #4e0eff;
+    }
+  }
+  span{
+    color : white;
+    text-transform : uppercase;
+    a {
+      color:#4e0eff;
+      text-decoration: none;
+      font-weight: bold;
+    }
+  }
 `;
 
 export default Register;
