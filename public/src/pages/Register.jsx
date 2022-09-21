d281c6f274d3261ea9d256f343cc08f3c81899df
yash <yashwable12@gmail.com>
2022-09-21 18:07:40 +0530

    used react-toster ,axios

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,8 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
-import Logo from "../assets/logo.svg"
-import { useState } from 'react';
+import Logo from "../assets/logo.svg";
+import {ToastContainer,toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+import {registerRoute} from "../utils/APIRoutes";
+
 
 function Register() {
   const [values,setValues] = useState({
@@ -12,10 +16,43 @@ function Register() {
     confirmPassword : ""
   })
 
-  const handleSubmit = (event) => {
+  const toastOptions = {
+    position:"bottom-right",
+    autoClose:8000,
+    pauseOnHover:true,
+    draggable:true,
+    theme:"dark"
+  }
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    alert('form');
+    if(handleValidation()){
+      const {password,confirmPassword ,username,email} = values;
+      const {data} = await axios.post(registerRoute,{
+        username,
+        email,
+        password
+      });
+    };
   };
+
+  const handleValidation =() => {
+    const {password,confirmPassword ,username,email} = values;
+    if (password !== confirmPassword) {
+      toast.error("password and confirm password should be same." ,toastOptions);
+      return false;
+    } else if (username.length <3) {
+      toast.error("Username should be greater than 3 characters." ,toastOptions);
+      return false;
+    } else if (password.length < 8) {
+      toast.error("password should be greater than or equal to 8 characters." ,toastOptions);
+      return false;
+    } else if (email === "") {
+      toast.error("email is required" ,toastOptions);
+      return false;
+    }
+    return true;
+  }
   
   const handleChange = (event) => {
     setValues({...values,[event.target.name] : event.target.value }); 
@@ -50,13 +87,14 @@ function Register() {
         <input 
           type = "password" 
           placeholder = "Password" 
-          name = "password" 
+          name = "confirmPassword" 
           onChange={e => handleChange(e)} 
         />
         <button>Create User</button>
         <span>Already have an account? <Link to = "/login">Login</Link></span>
       </form>
     </FormContainer>
+    <ToastContainer/>
     </> 
   ); 
 }
---
+++ public/src/utils/APIRoutes.js
@@ -0,0 +1,2 @@
+const host = "http://localhost:5000";
+export const registerRoutes = `${host}/api/auth/login`;
