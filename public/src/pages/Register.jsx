362fe86476cd9be8b6d40c087ce31668b5623446
yash <yashwable12@gmail.com>
2022-09-22 23:49:04 +0530

    completed with register form

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
-import { Link } from "react-router-dom";
+import { Link ,useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.svg";
 import {ToastContainer,toast} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
@@ -9,6 +9,7 @@ import {registerRoute} from "../utils/APIRoutes";
 
 
 function Register() {
+  const navigate = useNavigate();
   const [values,setValues] = useState({
     username : "",
     email : "",
@@ -27,12 +28,19 @@ function Register() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     if(handleValidation()){
-      const {password,confirmPassword ,username,email} = values;
+      const {password ,username,email} = values;
       const {data} = await axios.post(registerRoute,{
         username,
         email,
         password
       });
+      if (data.status === false){
+        toast.error(data.msg, toastOptions);
+      }
+      if (data.status === true){
+        localStorage.setItem("chat-app-user",JSON.stringify(data.user));
+      }
+      navigate("/");
     };
   };
 
